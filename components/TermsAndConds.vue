fa27b623fc00ec4b9af8901f6a477107d46bf756
luck21honey <lukeadamdev@gmail.com>
2020-11-04 07:07:27 +0000

    update terms page & create single product page

--- components/TermsAndConds.vue
+++ components/TermsAndConds.vue
@@ -3,127 +3,143 @@
   Tailwind UI components require Tailwind CSS v1.8 and the @tailwindcss/ui plugin.
   Read the documentation to get started: https://tailwindui.com/documentation
 -->
-  <section class="bg-white overflow-hidden">
-    <div
-      class="relative max-w-screen-xl mx-auto pt-20 pb-12 px-4 sm:px-6 lg:px-8 lg:py-20"
-    >
-      <svg
-        class="absolute top-full left-0 transform translate-x-80 -translate-y-24 lg:hidden"
-        width="784"
-        height="404"
-        fill="none"
-        viewBox="0 0 784 404"
-      >
-        <defs>
-          <pattern
-            id="e56e3f81-d9c1-4b83-a3ba-0d0ac8c32f32"
-            x="0"
-            y="0"
-            width="20"
-            height="20"
-            patternUnits="userSpaceOnUse"
-          >
-            <rect
-              x="0"
-              y="0"
-              width="4"
-              height="4"
-              class="text-gray-200"
-              fill="currentColor"
-            />
-          </pattern>
-        </defs>
-        <rect
-          width="784"
-          height="404"
-          fill="url(#e56e3f81-d9c1-4b83-a3ba-0d0ac8c32f32)"
-        />
-      </svg>
-
-      <svg
-        class="hidden lg:block absolute right-full top-1/2 transform translate-x-1/2 -translate-y-1/2"
-        width="404"
-        height="784"
-        fill="none"
-        viewBox="0 0 404 784"
-      >
-        <defs>
-          <pattern
-            id="56409614-3d62-4985-9a10-7ca758a8f4f0"
-            x="0"
-            y="0"
-            width="20"
-            height="20"
-            patternUnits="userSpaceOnUse"
-          >
-            <rect
-              x="0"
-              y="0"
-              width="4"
-              height="4"
-              class="text-gray-200"
-              fill="currentColor"
-            />
-          </pattern>
-        </defs>
-        <rect
-          width="404"
-          height="784"
-          fill="url(#56409614-3d62-4985-9a10-7ca758a8f4f0)"
-        />
-      </svg>
-
-      <div class="relative lg:flex lg:items-center">
-        <div class="hidden lg:block lg:flex-shrink-0">
-          <img
-            class="h-64 w-64 rounded-full xl:h-80 xl:w-80"
-            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-            alt=""
-          />
-        </div>
-
-        <div class="relative lg:ml-10">
+  <!-- This component requires Tailwind CSS >= 1.5.1 and @tailwindcss/ui >= 0.4.0 -->
+  <div class="bg-white overflow-hidden">
+    <div class="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+      <div
+        class="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen"
+      ></div>
+      <div class="mx-auto text-base max-w-prose lg:max-w-none">
+        <p
+          class="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase"
+        >
+          Case Study
+        </p>
+        <h1
+          class="mt-2 mb-8 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
+        >
+          Meet Whitney
+        </h1>
+      </div>
+      <div class="lg:grid lg:grid-cols-2 lg:gap-8">
+        <div class="relative mb-8 lg:mb-0 lg:row-start-1 lg:col-start-2">
           <svg
-            class="absolute top-0 left-0 transform -translate-x-8 -translate-y-24 h-36 w-36 text-indigo-200 opacity-50"
-            stroke="currentColor"
+            class="hidden lg:block absolute top-0 right-0 -mt-20 -mr-20"
+            width="404"
+            height="384"
             fill="none"
-            viewBox="0 0 144 144"
+            viewBox="0 0 404 384"
           >
-            <path
-              stroke-width="2"
-              d="M41.485 15C17.753 31.753 1 59.208 1 89.455c0 24.664 14.891 39.09 32.109 39.09 16.287 0 28.386-13.03 28.386-28.387 0-15.356-10.703-26.524-24.663-26.524-2.792 0-6.515.465-7.446.93 2.327-15.821 17.218-34.435 32.11-43.742L41.485 15zm80.04 0c-23.268 16.753-40.02 44.208-40.02 74.455 0 24.664 14.891 39.09 32.109 39.09 15.822 0 28.386-13.03 28.386-28.387 0-15.356-11.168-26.524-25.129-26.524-2.792 0-6.049.465-6.98.93 2.327-15.821 16.753-34.435 31.644-43.742L121.525 15z"
+            <defs>
+              <pattern
+                id="de316486-4a29-4312-bdfc-fbce2132a2c1"
+                x="0"
+                y="0"
+                width="20"
+                height="20"
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x="0"
+                  y="0"
+                  width="4"
+                  height="4"
+                  class="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width="404"
+              height="384"
+              fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)"
             />
           </svg>
-          <blockquote class="relative">
-            <div class="text-2xl leading-9 font-medium text-gray-900">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-                expedita voluptas culpa sapiente alias molestiae. Numquam
-                corrupti in laborum sed rerum et corporis.
-              </p>
-            </div>
-            <footer class="mt-8">
-              <div class="flex">
-                <div class="flex-shrink-0 lg:hidden">
-                  <img
-                    class="h-12 w-12 rounded-full"
-                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </div>
-                <div class="ml-4 lg:ml-0">
-                  <div class="text-base leading-6 font-medium text-gray-900">
-                    Judith Black
-                  </div>
-                  <div class="text-base leading-6 font-medium text-indigo-600">
-                    CEO, Tuple
-                  </div>
-                </div>
+          <div class="relative text-base mx-auto max-w-prose lg:max-w-none">
+            <figure>
+              <div class="relative pb-7/12 lg:pb-0">
+                <img
+                  src="https://images.unsplash.com/photo-1546913199-55e06682967e?ixlib=rb-1.2.1&auto=format&fit=crop&crop=focalpoint&fp-x=.735&fp-y=.55&w=1184&h=1376&q=80"
+                  alt=""
+                  width="1184"
+                  height="1376"
+                  class="rounded-lg shadow-lg object-cover object-center absolute inset-0 w-full h-full lg:static lg:h-auto"
+                />
               </div>
-            </footer>
-          </blockquote>
+              <figcaption class="flex mt-3 text-sm text-gray-500">
+                <!-- Heroicon name: camera -->
+                <svg
+                  class="flex-none w-5 h-5 mr-2 text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                Photograph by Marcus O’Leary
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+        <div>
+          <div class="text-base max-w-prose mx-auto lg:max-w-none">
+            <p class="text-lg leading-7 text-gray-500 mb-5">
+              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
+              urna sed consectetur neque tristique pellentesque. Blandit amet,
+              sed aenean erat arcu morbi.
+            </p>
+          </div>
+          <div
+            class="prose text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1"
+          >
+            <p>
+              Sollicitudin tristique eros erat odio sed vitae, consequat turpis
+              elementum. Lorem nibh vel, eget pretium arcu vitae. Eros eu
+              viverra donec ut volutpat donec laoreet quam urna.
+            </p>
+            <p>
+              Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique sit
+              nisi lorem pulvinar. Urna, laoreet fusce nibh leo. Dictum et et et
+              sit. Faucibus sed non gravida lectus dignissim imperdiet a.
+            </p>
+            <p>
+              Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem
+              arcu, metus, egestas netus cursus. In.
+            </p>
+            <ul>
+              <li>Quis elit egestas venenatis mattis dignissim.</li>
+              <li>
+                Cras cras lobortis vitae vivamus ultricies facilisis tempus.
+              </li>
+              <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+            </ul>
+            <p>
+              Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt
+              vel ultricies. Vulputate aliquet velit faucibus semper.
+              Pellentesque in venenatis vestibulum consectetur nibh id. In id ut
+              tempus egestas. Enim sit aliquam nec, a. Morbi enim fermentum
+              lacus in. Viverra.
+            </p>
+            <h2>How we helped</h2>
+            <p>
+              Tincidunt integer commodo, cursus etiam aliquam neque, et.
+              Consectetur pretium in volutpat, diam. Montes, magna cursus nulla
+              feugiat dignissim id lobortis amet. Laoreet sem est phasellus eu
+              proin massa, lectus. Diam rutrum posuere donec ultricies non
+              morbi. Mi a platea auctor mi.
+            </p>
+            <p>
+              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
+              urna sed consectetur neque tristique pellentesque. Blandit amet,
+              sed aenean erat arcu morbi.
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  </section>
+  </div>
 </template>
