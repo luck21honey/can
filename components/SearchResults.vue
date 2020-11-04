9a6ca38f33062691ac80bc17f72f48a08bd7c8ca
luck21honey <lukeadamdev@gmail.com>
2020-11-04 06:51:02 +0000

    update search page

--- components/SearchResults.vue
+++ components/SearchResults.vue
@@ -4,14 +4,14 @@
   Read the documentation to get started: https://tailwindui.com/documentation
 -->
   <div class="bg-gray-50">
-    <div class="max-w-screen-xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
+    <div class="max-w-screen-xl mx-auto py-3 px-4 sm:py-6 sm:px-6 lg:px-8">
       <div class="max-w-3xl mx-auto">
-        <h2
-          class="text-center text-3xl leading-9 font-extrabold text-gray-900 sm:text-4xl sm:leading-10"
+        <div
+          class="text-left leading-9 text-gray-900 sm:text-4xl sm:leading-10"
         >
-          Search Results
-        </h2>
-        <div class="mt-6 border-t-2 border-gray-200 pt-6">
+          Search Results: 6
+        </div>
+        <div class="mt-3 border-t-2 border-gray-200 pt-6">
           <dl>
             <div>
               <dt class="text-lg leading-7">
