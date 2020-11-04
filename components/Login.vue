af9fac4c6953997c07d88bbcdf28baccad5cb0d9
luck21honey <lukeadamdev@gmail.com>
2020-11-04 05:49:35 +0000

    update login page

--- components/Login.vue
+++ components/Login.vue
@@ -173,12 +173,18 @@
 
               <div>
                 <span class="block w-full rounded-md shadow-sm">
-                  <button
+                  <!-- <button
                     type="submit"
                     class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
                   >
                     Sign in
-                  </button>
+                  </button> -->
+                  <nuxt-link
+                    to="/home"
+                    class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+                  >
+                    Sign in
+                  </nuxt-link>
                 </span>
               </div>
             </form>
