130a42c00d686e294fba0d008193570974fe39a3
luck21honey <lukeadamdev@gmail.com>
2020-11-05 17:45:25 +0000

    update default layout for auth

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
+<div>
     <nuxt />
-  </div>
+</div>
 </template>
 
+<script>
+export default {
+    mounted() {
+        if ($store.getters.isAuthenticated) {
+            this.$router.push('/home');
+        }
+    }
+}
+</script>>
