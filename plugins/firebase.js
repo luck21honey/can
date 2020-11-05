f6fbafe7038d41cca04b820f7a50c54e38349a61
luck21honey <lukeadamdev@gmail.com>
2020-11-05 14:32:30 +0000

    update firebase js

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -5,13 +5,14 @@ import 'firebase/auth'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "Put your API here",
-    authDomain: "your-project-name.firebaseapp.com",
-    databaseURL: "https://your-project-name.firebaseio.com",
-    projectId: "your-project-name",
-    storageBucket: "your-project-name.appspot.com",
-    messagingSenderId: "Put your messaging sender ID here",
-    appId: "Put your app ID here"
+    apiKey: "AIzaSyCb8EyxN8EaPXht7eDoGGezFFh-rMRV6xI",
+    authDomain: "awards-box-jamstack-app.firebaseapp.com",
+    databaseURL: "https://awards-box-jamstack-app.firebaseio.com",
+    projectId: "awards-box-jamstack-app",
+    storageBucket: "awards-box-jamstack-app.appspot.com",
+    messagingSenderId: "920078951566",
+    appId: "1:920078951566:web:3660cf50f70236e1f355df",
+    measurementId: "G-1Y4XF9ZZZ2"
 };
 // Initialize Firebase
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : ''
