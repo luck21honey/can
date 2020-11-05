72f69e08f13befda860b00dc664070d8787bbd94
luck21honey <lukeadamdev@gmail.com>
2020-11-05 04:27:48 +0000

    update singleproduct, profile, settings with main layout

--- pages/terms/index.vue
+++ pages/terms/index.vue
@@ -2,7 +2,7 @@
 <main class="flex-1 relative overflow-y-auto focus:outline-none" tabindex="0">
     <div class="pt-2 pb-6 md:py-6">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-            <h1 class="text-2xl font-semibold text-gray-900">Products</h1>
+            <h1 class="text-2xl font-semibold text-gray-900">Terms & Conds</h1>
         </div>
         <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
             <!-- Replace with your content -->
